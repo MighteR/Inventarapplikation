ce6049d9f3f357839a8d3f724e39f15844b93df8
Dave <david.buchwalder@googlemail.com>
2012-11-19 18:47:49 +0100

    -Language Settings Changed + Category Language added

--- application/language/english/category_lang.php
+++ application/language/english/category_lang.php
@@ -1,4 +1,5 @@
 <?php
 $lang['title_category_list'] = 'Category List';
 $lang['title_category_name'] = 'Category Name';
+$lang['title_create_category'] = 'Create Category';
 ?>
--- application/language/german/category_lang.php
+++ application/language/german/category_lang.php
@@ -1,4 +1,5 @@
 <?php
 $lang['title_category_list'] = 'Kategorieliste';
 $lang['title_category_name'] = 'Kategoriename';
+$lang['title_create_category'] = 'Kategorie erstellen';
 ?>
