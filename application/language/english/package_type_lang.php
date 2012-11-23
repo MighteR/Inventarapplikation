a9c31ed2c40b304d72b8fa035c1fe1b44b214871
Dave <david.buchwalder@googlemail.com>
2012-11-23 16:38:37 +0100

    -Package_Type Language strings added

--- application/language/english/package_type_lang.php
+++ application/language/english/package_type_lang.php
@@ -2,6 +2,7 @@
 $lang['title_package_type_list'] = 'Package Type List';
 $lang['title_package_type_name'] = 'Package Type Name';
 $lang['title_create_package_type'] = 'Create Package Type';
-$lang['question_delete_package_type'] = 'Delete Package Type?';
+$lang['question_delete_package_type'] = 'Are you sure you want to delete this package type?';
+$lang['title_delete_package_type'] = 'Delete Package Type';
 
 ?>
--- application/language/german/package_type_lang.php
+++ application/language/german/package_type_lang.php
@@ -3,7 +3,8 @@
 $lang['title_package_type_list'] = 'Verpackungstypenliste';
 $lang['title_package_type_name'] = 'Verpackungstypenname';
 $lang['title_create_package_type'] = 'Verpackungstyp erstellen';
-$lang['question_delete_package_type'] = 'Verpackungstyp löschen?';
+$lang['question_delete_package_type'] = 'Wollen Sie diesen Verpackungstyp löschen?';
+$lang['title_delete_package_type'] = 'Verpackungstyp löschen';
 
 
 ?>
