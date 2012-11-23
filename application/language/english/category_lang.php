31eb945b857678cc59b0b740ed97ccc3252e326a
Dave <david.buchwalder@googlemail.com>
2012-11-23 13:05:07 +0100

    -Added Language strings

--- application/language/english/category_lang.php
+++ application/language/english/category_lang.php
@@ -2,4 +2,6 @@
 $lang['title_category_list'] = 'Category List';
 $lang['title_category_name'] = 'Category Name';
 $lang['title_create_category'] = 'Create Category';
+$lang['question_delete_category'] = 'Are you sure you want to delete this category?';
+$lang['title_delete_category'] = 'Delete category';
 ?>
--- application/language/german/category_lang.php
+++ application/language/german/category_lang.php
@@ -2,4 +2,6 @@
 $lang['title_category_list'] = 'Kategorieliste';
 $lang['title_category_name'] = 'Kategoriename';
 $lang['title_create_category'] = 'Kategorie erstellen';
+$lang['question_delete_category'] = 'Wollen Sie diese Kategorie wirkloch löschen?';
+$lang['title_delete_category'] = 'Kategorie löschen';
 ?>
