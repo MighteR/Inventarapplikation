ef33d8e0032cc2c3580472b8b18d242c20b1a0a4
Dave <david.buchwalder@googlemail.com>
2012-11-30 15:25:46 +0100

    - Class Product changed

--- application/language/english/product_lang.php
+++ application/language/english/product_lang.php
@@ -3,6 +3,8 @@
 $lang['title_product_list'] = 'Product List';
 $lang['title_product_name'] = 'Product Name';
 $lang['title_create_product'] = 'Create Product';
+$lang['title_delete_product'] = 'Delete Product';
 $lang['title_product'] = 'Product';
+$lang['info_package_type_created'] = 'Product has been created successfully';
 
 ?>
--- application/language/german/product_lang.php
+++ application/language/german/product_lang.php
@@ -3,6 +3,8 @@
 $lang['title_product_list'] = 'Produktliste';
 $lang['title_product_name'] = 'Produktname';
 $lang['title_create_product'] = 'Produkt erstellen';
+$lang['title_delete_product'] = 'Produkt löschen';
 $lang['title_product'] = 'Produkt';
+$lang['info_package_type_created'] = 'Produkt erfolgreich erstellt';
 
 ?>
