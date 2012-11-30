ed9d1c9d0ca0c3a8ac54d580f3cf5760d4c3b676
Dave <david.buchwalder@googlemail.com>
2012-11-30 15:21:45 +0100

    - Class Product changed

--- application/controllers/product.php
+++ application/controllers/product.php
@@ -33,8 +33,38 @@ class Product extends MY_Controller {
         $this->template->render();
       }
       
-      public function indexList($page = 1){
-          
-      }
+    public function indexList($page = 1){
+        if($this->session->userdata('admin')){
+            if($this->input->is_ajax_request() AND !empty($_POST)){
+                $p_name = $this->input->post('name');
+                $p_page_output = $this->input->post('page_output');
+
+                $this->load->model('product_model');
+                $this->load->library('pages');
+                
+                $query = $this->product_model->get_product_by_name($p_name);
+
+                if($query->num_rows() > 0){
+                    $this->pages->check_page($query->num_rows(),$page,true,$p_page_output);
+
+                    $query = $this->product_model->get_product_by_name($p_name,$this->pages->get_limit());
+                    $data['package_types'] = $query->result_object();
+
+                    $data['entry'] = true;
+                }else{
+                    $data['entry'] = false;
+                }                
+
+                $content = $this->load->view('product/index_list',$data,true);
+                echo $content;
+            }
+            exit();
+        }else{
+            $this->load->library('messages');
+            $this->messages->get_message('error',$this->lang->line('error_no_access'));
+        }
+        
+        $this->template->render();
+    }
 }
 ?>
--- application/views/product/index.php
+++ application/views/product/index.php
@@ -2,16 +2,92 @@
 <script type="text/javascript" src="<?php echo base_url('application/views/template/js/jquery-ui-1.9.1.custom.min.js'); ?>"></script>
 <script type="text/javascript">
     
+    $(document).ready(function(){
+    $('button').button();
+    //predefine_user_search('');
+
+    $('#create').click(function(){
+            window.location.href = '<?php echo current_url(); ?>/create';
+    });
+	
+    $(document).keypress(function(e){ 
+        var element = e.target.nodeName.toLowerCase(); 
+        if (e.keyCode == 13 && element != 'textarea'){ 
+                return false; 
+        }
+    });
+    
+    $("input[id*='search']").keypress(function(e){
+        if(e.which == 13){
+            search_product();
+            $(this).focus();
+            var val = $(this).val();        
+            $(this).val(''); 
+            $(this).val(val);
+        }
+    });
+    
+    $('#search_product').click(function(){
+        search_product();
+    });
+    
+    $('#reset_search_product').click(function(){
+        predefine_product_search('');
+    });
+});
+
+function predefine_product_search(name){
+    $('#search_name').val(name);
+
+    search_product();
+}
+function search_product(page){
+    var search_name = $('#search_name').val();
+
+    if(typeof page === 'undefined'){
+        page = '';
+    }
+
+    $('#loader').dialog({
+            closeOnEscape: false,
+            dialogClass: 'loader',
+            height: 50,
+            resizable: false,
+            width: 50
+    });
+
+    if($('#product_output').val() == 'undefined'){
+        output = 0;
+    }else{
+        output = $('#product_output').val();
+    }
+
+    $.ajax({
+        complete: function(html){
+            $('#loader').dialog('close');
+        },
+        url: '<?php echo current_url(); ?>/indexList' + page,
+        type: 'POST',
+        data: {
+            'name': search_name,
+            'page_output': output
+        },
+        success: function(html){
+            $('#product').html(html);
+        }
+    });
+}
+    
 </script>
 <div id="content_title">
     <span><?php echo lang('title_product_list'); ?></span>
 </div>
 <div class="first">
     <div class="text_left">
-        <?php echo lang('title_product_name','search_title_product_name'); ?>
+        <?php echo lang('title_product_name','search_product_name'); ?>
     </div>
     <div class="text_right">
-        <input class="formular" id="search_name" name="search_name" size="50" type="text" />
+        <input class="formular" id="search_name" name="search_product" size="50" type="text" />
     </div>
 </div>
 <div class="second">
@@ -19,11 +95,11 @@
         &nbsp;
     </div>
     <div class="text_right">
-        <button name="create" type="button" id="search_package_type" ><?php echo lang('title_submit'); ?></button>
-        <button name="create" type="button" id="reset_search_package_type" ><?php echo lang('title_reset'); ?></button>
+        <button name="create" type="button" id="search_product" ><?php echo lang('title_submit'); ?></button>
+        <button name="create" type="button" id="reset_search_product" ><?php echo lang('title_reset'); ?></button>
     </div>
 </div>
 <div class="text_title" style="text-align:center">
     <div><button name="create" type="button" id="create" ><?php echo lang('title_create_product'); ?></button></div>
 </div>
-<div id="package_type"></div>
+<div id="product"></div>
